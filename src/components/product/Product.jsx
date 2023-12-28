fffaf51f05a0e694ef393e3901a5f6fdaf9bf04f
Bhoomika <bhoomikahm18@gmail.com>
2023-12-28 12:42:47 +0530

    Function for cart items

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 
 import "./Product.css";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../../store/cart_slice";
 
-const Product = ({ name, id, imgURL, price }) => {
+function Product(props) {
+  const cartItems = useSelector((state) => state.cart.itemList);
+  const dispatch = useDispatch();
+  function addToCart() {
+    dispatch(
+      cartActions.addtoCart({
+        name: props.name,
+        id: props.id,
+        price: props.price,
+      })
+    );
+  }
   return (
     <div className="card">
-      <img src={imgURL} alt={name} />
-      <h2>{name}</h2>
-      <p>$ {price}</p>
-      <button>Add to cart</button>
+      <img src={props.imgURL} alt={props.name} />
+      <h2>{props.name}</h2>
+      <p>$ {props.price}</p>
+      <button onClick={addToCart}>Add to cart</button>
     </div>
   );
-};
+}
 
 export default Product;
---
+++ src/store/cart_slice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    itemList: [],
+    totalQuantity: 0,
+    showCart: false,
+  },
+  reducers: {
+    addtoCart(state, action) {
+      const newItem = action.payload;
+      //to check whether it is available
+      const existingItem = state.itemList.find(
+        (item) => item.id === newItem.id
+      );
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.price += newItem.price;
+      } else {
+        state.itemList.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalQuantity: newItem.price,
+          name: newItem.name,
+        });
+      }
+    },
+    removeFromCart(state) {},
+    setShowCart(state) {
+      state.showCart = true;
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
