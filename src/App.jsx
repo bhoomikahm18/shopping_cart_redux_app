7256dde56aa70695af8576708a115630abd45d1c
Bhoomika <bhoomikahm18@gmail.com>
2024-01-06 15:45:51 +0530

    Adding data to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/auth/Auth.jsx";
 import Layout from "./components/layout/Layout.jsx";
+import { useEffect } from "react";
 
 function App() {
+  const cart = useSelector(state => state.cart)
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemList);
-  console.log(cartItems);
+
+  useEffect(() => {
+    fetch('https://redux-http-e78a2-default-rtdb.firebaseio.com/cartItem.json',{
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  },[cart])
+  
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
